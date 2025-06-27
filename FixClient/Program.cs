cd8961ec5a92f305133c0f142319e3294fcfc50f
khanh-dao-alpha <khanh.dao@alphalab.capital>
2025-06-27 15:05:47 +0700

    inital commit

---
+++ FixClient/Program.cs
@@ -0,0 +1,74 @@
+﻿using Epam.FixAntenna.Constants.Fixt11; 
+using Epam.FixAntenna.NetCore.FixEngine;
+using Epam.FixAntenna.NetCore.FixEngine.Session.Util;
+using Epam.FixAntenna.NetCore.Message;
+
+namespace Client
+{
+  public class SimpleNewsBroadcaster
+  {
+	public static void Main(string[] args)
+	{
+		var sessionList = new List<IFixSession>();
+		
+		var sessions = SessionParametersBuilder.BuildSessionParametersList("./fixclient.properties");
+		
+		foreach (var kvp in sessions)
+		{
+			var sessionId = kvp.Key;
+			var details = kvp.Value;
+		
+			Console.WriteLine($"=== Session: {sessionId} ===");
+		
+			var session = details.CreateInitiatorSession();
+			sessionList.Add(session);
+			session.SetFixSessionListener(new MyFixSessionListener(session));
+		
+			session.Connect();
+		}
+		
+		
+		foreach (var session in sessionList)
+		{
+			FixMessage messageContent = new FixMessage();
+			messageContent.AddTag(148, "Hello there");
+			messageContent.AddTag(33, 3);
+			messageContent.AddTag(58, "line1");
+			messageContent.AddTag(58, "line2");
+			messageContent.AddTag(58, "line3");
+		
+			session.SendMessage("B", messageContent);
+		}
+		
+		Console.Read();
+		foreach (var session in sessionList)
+		{
+			if (SessionState.IsNotDisconnected(session.SessionState))
+			{
+				session.Disconnect("Shutting down...");
+			}
+			session.Dispose();
+		}
+
+	}
+
+	private class MyFixSessionListener(IFixSession session) : IFixSessionListener
+	{
+		public void OnNewMessage(FixMessage message)
+	  {
+		Console.WriteLine($"{session.Parameters.SessionId}: New application level message type: " + message.GetTagValueAsString(Tags.MsgType) + "received");
+	  }
+
+	  public void OnSessionStateChange(SessionState sessionState)
+	  {
+		Console.WriteLine(session.Parameters.SenderCompId + " <-> " + session.Parameters.TargetCompId + "---" +
+		                  $"{session.Parameters.SessionId}: Session state changed:" + sessionState);
+		Console.WriteLine();
+		if (sessionState == SessionState.Disconnected)
+		{
+		  session.Dispose();
+		}
+	  }
+	}
+  }
+}
---
+++ FixServerTest/Program.cs
@@ -0,0 +1,64 @@
+﻿using Epam.FixAntenna.NetCore.Configuration;
+using Epam.FixAntenna.NetCore.FixEngine;
+using Epam.FixAntenna.NetCore.Message;
+
+namespace Server
+{
+  public class SimpleServer : IFixServerListener
+  {
+	public static void Main(string[] args)
+	{
+		var configuration = new Config("./fixengine.properties");
+	    FixServer server = new FixServer(configuration); 
+	    server.SetListener(new SimpleServer());
+	  
+	    server.Start(); 
+
+		Console.Read(); 
+		server.Stop(); 
+	}
+
+	public void NewFixSession(IFixSession session)
+	{
+	  try
+	  {
+		session.SetFixSessionListener(new MyFixSessionListener(session)); 
+		
+		session.Connect();
+		Console.WriteLine("New FIX session accepted: " +
+		                  session.Parameters.SenderCompId + " <-> " +
+		                  session.Parameters.TargetCompId);	  }
+	  catch (IOException e)
+	  {
+		  Console.WriteLine(e.Message);
+	  }
+	}
+
+	private class MyFixSessionListener(IFixSession session) : IFixSessionListener
+	{
+	  public void OnSessionStateChange(SessionState sessionState)
+	  {
+		Console.WriteLine("Session state: " + sessionState);
+
+		if (sessionState == SessionState.Disconnected)
+		{
+		  session.Dispose();
+		  Console.WriteLine("Your session has been disconnected. Press ENTER to exit the programm.");
+		}
+	  }
+
+	  public void OnNewMessage(FixMessage message)
+	  {
+		Console.WriteLine("New message is accepted: " + message.ToString());
+		var ack = new FixMessage();
+		ack.AddTag(35, "8"); // MsgType = ExecutionReport
+		ack.AddTag(150, "0"); // OrdStatus = New
+		ack.AddTag(39, "0");  // OrdStatus = New
+		ack.AddTag(37, "12345"); // OrderID
+		ack.AddTag(17, "1");     // ExecID
+
+		session.SendMessage("8", ack);
+	  }
+	}
+  }
+}
